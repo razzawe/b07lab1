fa4223d10d66297cf423ccfd7c51e7fcde661302
Rami <rami.alazzawe@outlook.com>
2024-05-13 16:23:33 -0400

    added Polynomial.java and Driver.java

--- Polynomial.java
+++ Polynomial.java
@@ -12,28 +12,8 @@ public class Polynomial{
     }
 
 
-    // public Polynomial add(Polynomial poly){
-    //     int length = poly.coeff.length + this.coeff.length;
-    //     System.out.println("the length is: "+ length); // test
-    //     double [] x = new double[length];
-    //     System.out.println("Length x:" + x.length); // test
-    //     for (int i = 0; i<this.coeff.length; i++){
-    //         x[i] = this.coeff[i];
-    //         // System.out.println(x[i]); // test
-    //     }
-    //     // System.out.println(x.length); // test
-    //     for (int i = this.coeff.length; i<length; i++){
-    //         x[i] = poly.coeff[i-(this.coeff.length)];
-    //     }
-    //     poly.coeff = x;
-    //     for (int i = 0; i<length; i++){
-    //         System.out.println(poly.coeff[i]); // test
-    //     }
-    //     return poly;
-    // }
     public Polynomial add(Polynomial poly){
         int length = poly.coeff.length + this.coeff.length;
-        System.out.println("the length is: "+ length); // test
         double [] x = new double[length];
 
         for (int i = 0; i<length; i++){
@@ -48,9 +28,6 @@ public class Polynomial{
             }
         }
         poly.coeff = x;
-        for (int i = 0; i<length; i++){
-            System.out.println(poly.coeff[i]); // test
-        }
         return poly;
     }
 
